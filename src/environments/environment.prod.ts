98e5c9f9d28799b00203adb2e5161c48d7113322
Mathew Sachin <mathew.sachin.git@outlook.com>
2018-03-16 22:02:09 +0530

    update production server url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   razorPay: "rzp_test_glA246D8rCFOVc",
-  apiUrl:  "https://bitnami-zf9s8bac8w.appspot.com/api",
-  uploadsBaseUrl:  "https://bitnami-zf9s8bac8w.appspot.com"
+  apiUrl:  "https://zaaa-server.appspot.com/api",
+  uploadsBaseUrl:  "https://zaaa-server.appspot.com"
 };
