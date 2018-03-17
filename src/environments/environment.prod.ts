626934383862acccc822db0f32bc59d73ee55f74
Mathew Sachin <mathew.sachin.git@outlook.com>
2018-03-17 20:16:16 +0530

    update razorpay key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  razorPay: "rzp_test_glA246D8rCFOVc",
+  razorPay: "rzp_test_qIUnr51XOjxMYX",
   apiUrl:  "https://zaaa-server.appspot.com/api",
   uploadsBaseUrl:  "https://zaaa-server.appspot.com"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  razorPay: "rzp_test_glA246D8rCFOVc",
+  razorPay: "rzp_test_qIUnr51XOjxMYX",
   apiUrl: "http://localhost:8080/api",
   uploadsBaseUrl: "http://localhost:8080"
 };
