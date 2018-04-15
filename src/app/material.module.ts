ebf89f61257b92e705dd140ffe6914db44beb3bc
Mathew Sachin <mathew.sachin.git@outlook.com>
2018-04-15 15:00:18 +0530

    ratecard, releaseorder list menu

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -14,6 +14,7 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 const modules = [
   MatCheckboxModule,
@@ -29,7 +30,8 @@ const modules = [
   MatDialogModule,
   MatButtonModule,
   MatSlideToggleModule,
-  MatIconModule
+  MatIconModule,
+  MatMenuModule
 ]
 
 @NgModule({
