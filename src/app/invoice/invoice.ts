6f1ce1853b63bf769e2e307bd0b3e8fea0eee02e
Mathew Sachin <mathew.sachin.git@outlook.com>
2018-05-17 22:27:52 +0530

    t

--- src/app/invoice/invoice.ts
+++ src/app/invoice/invoice.ts
@@ -1,3 +1,42 @@
+import { OtherCharges, Insertion } from "../release-order/release-order";
+
 export class Invoice {
-    releaseOrder: string;
+    date: Date;
+
+    adGrossAmount = 0;
+    
+    taxType: 'CGST' | 'IGST' | 'SGST';
+    
+    taxAmount = {
+        primary: 0,
+        secondary: 0
+    }
+
+    taxIncluded = false;
+
+    otherCharges: OtherCharges[] = [];
+
+    extraCharges = 0;
+
+    publicationDiscount = 0;
+    agencyDiscount1 = 0;
+    agencyDiscount2 = 0;
+    
+    additionalCharges = 0;
+
+    additionalTax = 0;
+
+    caption = '';
+    remark = '';
+    otherRemark = '';
+    FinalAmount = 0;
+    FinalTaxAmount = 0;
+    netAmountFigures = 0;
+    netAmountWords = '';
+
+    /*for payment reciept */
+    clearedAmount = 0;
+    pendingAmount = 0;
+
+    insertions: Insertion[] = [];
 }
