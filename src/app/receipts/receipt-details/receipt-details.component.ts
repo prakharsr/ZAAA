8925a4b2a295e5640ad7237835e4b62dc88c30dd
Prakhar Sharma <prakhar03sharma@gmail.com>
2018-05-20 12:23:03 +0530

    erroneousPush

--- src/app/receipts/receipt-details/receipt-details.component.ts
+++ src/app/receipts/receipt-details/receipt-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PaymentReceipt } from '../payment-receipt';
+import { ReceiptsApiService } from '../receipts-api.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-receipt-details',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReceiptDetailsComponent implements OnInit {
 
-  constructor() { }
+  receipt = new PaymentReceipt();
+
+  constructor(private api: ReceiptsApiService,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.data.subscribe((data: { receipt: PaymentReceipt }) => {
+      this.receipt = data.receipt;
+    });
   }
 
 }
