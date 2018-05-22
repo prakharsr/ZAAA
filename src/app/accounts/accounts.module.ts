14d830e25a933a8c668045deb26c314349b9621e
Mathew Sachin <mathew.sachin.git@outlook.com>
2018-05-22 16:17:43 +0530

    ro executive above

--- src/app/accounts/accounts.module.ts
+++ src/app/accounts/accounts.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BaseModule } from '../base.module';
+import { AccountsRoutingModule } from '@aaman/accounts/accounts-routing.module';
 
 @NgModule({
   imports: [
-    BaseModule
+    BaseModule,
+    AccountsRoutingModule
   ],
   declarations: []
 })
