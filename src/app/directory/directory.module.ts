a2692634a777c1b2c2b25096b713c20bbcf5bef4
Mathew Sachin <mathew.sachin.git@outlook.com>
2018-05-23 17:48:04 +0530

    directory index.ts

--- src/app/directory/directory.module.ts
+++ src/app/directory/directory.module.ts
@@ -1,30 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BaseModule } from '../base.module';
 
-import { DirRoutingModule } from '@aaman/dir/dir-routing.module';
+import {
+  DirComponent,
+  DirRoutingModule,
 
-import { DirComponent } from '@aaman/dir/dir/dir.component';
+  ClientListComponent,
+  ClientComponent,
+  ClientDetailsComponent,
+  ClientResolver,
+  ClientListResolver,
+  ClientApiService,
 
-import { ClientComponent } from '@aaman/dir/clients/client/client.component';
-import { ClientListComponent } from '@aaman/dir/clients/client-list/client-list.component';
-import { ClientDetailsComponent } from '@aaman/dir/clients/client-details/client-details.component';
-import { ClientApiService } from '@aaman/dir/clients/client-api.service';
-import { ClientResolver } from '@aaman/dir/clients/client-resolver.service';
-import { ClientListResolver } from '@aaman/dir/clients/client-list-resolver.service';
+  ExecutiveListComponent,
+  ExecutiveComponent,
+  ExecutiveDetailsComponent,
+  ExecutiveResolver,
+  ExecutiveListResolver,
+  ExecutiveApiService,
 
-import { ExecutiveComponent } from '@aaman/dir/executives/executive/executive.component';
-import { ExecutiveListComponent } from '@aaman/dir/executives/executive-list/executive-list.component';
-import { ExecutiveDetailsComponent } from '@aaman/dir/executives/executive-details/executive-details.component';
-import { ExecutiveApiService } from '@aaman/dir/executives/executive-api.service';
-import { ExecutiveResolver } from '@aaman/dir/executives/executive-resolver.service';
-import { ExecutiveListResolver } from '@aaman/dir/executives/executive-list-resolver.service';
-
-import { MediaHouseComponent } from '@aaman/dir/media-houses/media-house/media-house.component';
-import { MediaHouseListComponent } from '@aaman/dir/media-houses/media-house-list/media-house-list.component';
-import { MediaHouseDetailsComponent } from '@aaman/dir/media-houses/media-house-details/media-house-details.component';
-import { MediaHouseApiService } from '@aaman/dir/media-houses/media-house-api.service';
-import { MediaHouseResolver } from '@aaman/dir/media-houses/media-house-resolver.service';
-import { MediaHouseListResolver } from '@aaman/dir/media-houses/media-house-list-resolver.service';
+  MediaHouseListComponent,
+  MediaHouseComponent,
+  MediaHouseDetailsComponent,
+  MediaHouseResolver,
+  MediaHouseListResolver,
+  MediaHouseApiService
+} from '.';
 
 @NgModule({
   imports: [
---
+++ src/app/validators/index.ts
@@ -0,0 +1,6 @@
+export * from './verify-email.directive';
+export * from './verify-equals.directive';
+export * from './verify-fix-length.directive';
+export * from './verify-max.directive';
+export * from './verify-min.directive';
+export * from './verify-multiple-of.directive';
