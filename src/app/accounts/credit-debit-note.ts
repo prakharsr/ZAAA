a9dbfb90177f30335bc8e5b970a93d1d19b84896
Mathew Sachin <mathew.sachin.git@outlook.com>
2018-05-28 11:36:14 +0530

    add credit debit note model

---
+++ src/app/accounts/credit-debit-note.ts
@@ -0,0 +1,13 @@
+export class CreditDebitNote {
+    type = 0;
+    partyName = "";
+    docNumber = "";
+    amount = 0;
+    remark = "";
+    user = "";
+    date = {
+        day: 0,
+        month: 0,
+        year: 0
+    }
+}
