68fb95f1ac7d4ae53a24a538c9b1ee1dc4e20259
Prakhar Sharma <prakhar03sharma@gmail.com>
2018-06-06 12:23:45 +0530

    bru

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from 'app/services';
+import { ApiService, NotificationService } from 'app/services';
 import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 import { UserProfile } from '../../models';
 
@@ -12,10 +12,15 @@ export class NavbarComponent implements OnInit {
 
   isNavbarCollapsed = true;
   admin : boolean;
-  pageUrl;
+  pageUrl="";
   expectedUrl = "/register";
 
-  constructor(public api: ApiService, private router: Router, private route: ActivatedRoute) { }
+  emailOrPhone: string;
+  password: string;
+
+  hidePassword = true;
+
+  constructor(public api: ApiService, private router: Router, private route: ActivatedRoute, private notifications: NotificationService) { }
 
   ngOnInit() {
     this.router.events.subscribe(event => {
@@ -23,6 +28,7 @@ export class NavbarComponent implements OnInit {
         this.pageUrl = event.urlAfterRedirects;
       } 
     });
+    console.log(this.pageUrl);
   }
 
   collapseNavbar() {
@@ -31,5 +37,20 @@ export class NavbarComponent implements OnInit {
 
   toggleNavbar() {
     this.isNavbarCollapsed = !this.isNavbarCollapsed;
+  }
+
+  loginSubmit() {
+    this.api.login(this.emailOrPhone, this.password).subscribe(
+      data => {
+        if (data.success) {
+          this.router.navigateByUrl('dashboard');
+        }
+        else {
+          console.log(data);
+
+          this.notifications.show(data.msg);
+        }
+      }
+    );
   }
 }
