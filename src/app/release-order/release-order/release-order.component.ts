480c3c8c57bd956a339fe935039e3f043385c066
Prakhar Sharma <prakhar03sharma@gmail.com>
2018-06-13 14:58:25 +0530

    fgegg

--- src/app/release-order/release-order/release-order.component.ts
+++ src/app/release-order/release-order/release-order.component.ts
@@ -43,6 +43,8 @@ export class ReleaseOrderComponent implements OnInit {
   id: string;
 
   selectedCategories: Category[] = [null, null, null, null, null, null];
+  categories: Category[];
+  fixedCategoriesLevel = -1;
 
   constructor(private route: ActivatedRoute,
     private router: Router,
@@ -96,6 +98,8 @@ export class ReleaseOrderComponent implements OnInit {
   }
   
   ngOnInit() {
+    this.categories = this.options.categories;
+
     this.route.paramMap.subscribe(params => {
       if (params.has('id')) {
         this.id = params.get('id');
@@ -139,6 +143,15 @@ export class ReleaseOrderComponent implements OnInit {
   private initFromReleaseOrder() {
     this.route.data.subscribe((data: { releaseOrder: ReleaseOrder }) => {
       this.releaseorder = data.releaseOrder;
+
+      this.buildCategoryTree([
+        this.releaseorder.adCategory1,
+        this.releaseorder.adCategory2,
+        this.releaseorder.adCategory3,
+        this.releaseorder.adCategory4,
+        this.releaseorder.adCategory5,
+        this.releaseorder.adCategory6
+      ]);
 
       let insertionBkp = this.releaseorder.insertions;
 
@@ -226,6 +239,15 @@ export class ReleaseOrderComponent implements OnInit {
       }
       else this.customScheme = true;
 
+      for (let i = 0; i < rateCard.categories.length; ++i) {
+        if (rateCard.categories[i]) {
+          ++this.fixedCategoriesLevel;
+        }
+        else break;
+      }
+
+      this.buildCategoryTree(rateCard.categories);
+
       this.mediaHouseApi.searchMediaHouses(rateCard.mediaHouseName).subscribe(data => {
         if (data && data.length > 0) {
           this.mediaHouse = data[0];
@@ -235,6 +257,143 @@ export class ReleaseOrderComponent implements OnInit {
       });
     }
   }
+
+  private buildCategoryTree(categories: string[]) {
+    let c : Category = this.categories.find(p => p.name == categories[0]);
+
+    if (c) {
+      this.category1 = c;
+
+      let i = 1;
+
+      while (i < categories.length && c.subcategories.length > 0) {
+        c = c.subcategories.find(p => p.name == categories[i]);
+
+        if (c) {
+          this.setCategory(i, c);
+
+          ++i;
+        }
+        else break;
+      }
+    }
+  }
+
+  findSubCategories(category: Category, query: string): Category[] {
+    let result : Category[] = [];
+
+    if (category.name.toLowerCase().indexOf(query.toLowerCase()) != -1) {
+      result.push(category);
+    }
+
+    if (category.subcategories) {
+      category.subcategories.forEach(subCategory => {
+        this.findSubCategories(subCategory, query).forEach(a => result.push(a));
+      });
+    }
+
+    return result;
+  }
+
+  findCategories(query: string): Category[]  {
+    let result : Category[] = [];
+
+    if (query) {
+      let base = this.categories;
+
+      if (this.fixedCategoriesLevel > -1) {
+        base = this.selectedCategories[this.fixedCategoriesLevel].subcategories;
+      }
+
+      base.forEach(element => {
+        this.findSubCategories(element, query).forEach(a => result.push(a));
+      });
+    }
+
+    return result;
+
+  }
+
+  searchCategories = (text: Observable<string>) => {
+    return text.debounceTime(300)
+      .distinctUntilChanged()
+      .pipe(
+        map(term => this.findCategories(term))
+      )
+      .catch(() => of([]));
+  }
+
+  categoryInputFormatter = (result: Category) => {
+    let stack : Category[] = [];
+
+    while (result) {
+      stack.push(result);
+      result = result.parent;
+    }
+
+    let j = this.fixedCategoriesLevel + 1;
+
+    while (j > 0) {
+      stack.pop();
+
+      --j;
+    }
+
+    let i = this.fixedCategoriesLevel + 1;
+
+    while (stack.length) {
+      this.setCategory(i, stack.pop());
+
+      ++i;
+    }
+  }
+
+  categoryResultFormatter = (result: Category) => {
+    let stack : Category[] = [];
+
+    while (result) {
+      stack.push(result);
+      result = result.parent;
+    }
+
+    let formatted = stack.pop().name;
+
+    while (stack.length) {
+      formatted += " > " + stack.pop().name;
+    }
+
+    return formatted;
+  }
+
+  getCategory(index: number) {
+    return this.selectedCategories[index];
+  }
+
+  setCategory(index: number, category: Category) {
+    if (this.selectedCategories[index] == category) {
+      return;
+    }
+
+    this.selectedCategories[index] = category;
+
+    for (let i = index + 1; i < this.selectedCategories.length; ++i) {
+      this.setCategory(i, null);
+    }
+  }
+
+  get category1() { return this.getCategory(0); }
+  get category2() { return this.getCategory(1); }
+  get category3() { return this.getCategory(2); }
+  get category4() { return this.getCategory(3); }
+  get category5() { return this.getCategory(4); }
+  get category6() { return this.getCategory(5); }
+
+  set category1(category: Category) { this.setCategory(0, category); }
+  set category2(category: Category) { this.setCategory(1, category); }
+  set category3(category: Category) { this.setCategory(2, category); }
+  set category4(category: Category) { this.setCategory(3, category); }
+  set category5(category: Category) { this.setCategory(4, category); }
+  set category6(category: Category) { this.setCategory(5, category); }
 
   getCategories() {
     this.dialog.getCategoriesDetails().subscribe(data => {
