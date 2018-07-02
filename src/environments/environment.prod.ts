59576b1decd239aa186b9a99fb51fb3313f8f7c7
Prakhar Sharma <prakhar03sharma@gmail.com>
2018-07-02 19:28:41 +0530

    cr inv btns

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   razorPay: "rzp_test_86QLf2LFy65g2j",
-  apiUrl:  "https://aaman-208409.appspot.com/api",
-  uploadsBaseUrl:  "https://aaman-208409.appspot.com"
+  apiUrl:  "https://adagencymanager.com/api",
+  uploadsBaseUrl:  "https://adagencymanager.com"
 };
