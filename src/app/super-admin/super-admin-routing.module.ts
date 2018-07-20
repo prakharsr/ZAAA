04799f6ca1cdb5c5353d5c280692a5be3bb486c7
Mathew Sachin <mathew.sachin.git@outlook.com>
2018-07-20 18:36:59 +0530

    global media house list

--- src/app/super-admin/super-admin-routing.module.ts
+++ src/app/super-admin/super-admin-routing.module.ts
@@ -4,15 +4,31 @@ import { AdCategoriesComponent } from './ad-categories/ad-categories.component';
 import { SuperAdminGuard } from './super-admin-guard.service';
 import { SuperAdminDashboardComponent } from './super-admin-dashboard/super-admin-dashboard.component';
 import { SuperAdminLoginComponent } from './super-admin-login/super-admin-login.component';
+import { MediaHouseListComponent, MediaHouseListResolver } from 'app/directory';
 
 const routes: Routes = [
   {
     path: '',
+    data: { superAdmin: true },
     canActivate: [SuperAdminGuard],
     children: [
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
       { path: 'dashboard', component: SuperAdminDashboardComponent },
-      { path: 'categories', component: AdCategoriesComponent }
+      { path: 'categories', component: AdCategoriesComponent },
+      {
+        path: 'media_houses',
+        data: { global: true },
+        children: [
+          { path: '', redirectTo: 'list/1', pathMatch: 'full' },
+          {
+            path: 'list/:page',
+            component: MediaHouseListComponent,
+            resolve: {
+              list: MediaHouseListResolver
+            }
+          }
+        ]
+      }
     ]
   },
   { path: 'login', component: SuperAdminLoginComponent }
