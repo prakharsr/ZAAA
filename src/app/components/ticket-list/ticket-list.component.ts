c8a0051b375d5cf008f9ba53ebef8bcb3db36bac
Mathew Sachin <mathew.sachin.git@outlook.com>
2018-07-28 14:51:53 +0530

    ticket list pagination

--- src/app/components/ticket-list/ticket-list.component.ts
+++ src/app/components/ticket-list/ticket-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'app/services';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Ticket } from '../../models';
 
 @Component({
   selector: 'app-ticket-list',
@@ -8,12 +10,27 @@ import { ApiService } from 'app/services';
 })
 export class TicketListComponent implements OnInit {
 
-  res = null;
+  list: Ticket[] = [];
+  page = 0;
+  pageCount = 0;
 
-  constructor(private api: ApiService) { }
+  constructor(private api: ApiService,
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
-    this.res = this.api.queryTickets(1, 0);
+    this.route.paramMap.subscribe(params => {
+      this.page = Number(params.get('page'));
+
+      this.api.queryTickets(1, 0).subscribe(data => {
+        this.list = data.list;
+        this.pageCount = data.pageCount;
+      });
+    });
+  }
+
+  navigate(i: number) {
+    this.router.navigate(['/tickets/list', i]);
   }
 
 }
