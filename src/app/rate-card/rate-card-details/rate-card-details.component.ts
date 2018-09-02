865d328b6cd680068b583afca8e74caa40c500e7
Mathew Sachin <mathew.sachin.git@outlook.com>
2018-09-03 00:04:06 +0530

    rc details btns

--- src/app/rate-card/rate-card-details/rate-card-details.component.ts
+++ src/app/rate-card/rate-card-details/rate-card-details.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { RateCard } from '../rate-card';
 import { RateCardApiService } from '../rate-card-api.service';
+import { DialogService, NotificationService } from 'app/services';
+import { Observable } from 'rxjs/Observable';
+import { SuperAdminApiService } from 'app/super-admin/super-admin-api.service';
 
 @Component({
   selector: 'app-rate-card-details',
@@ -12,12 +15,21 @@ export class RateCardDetailsComponent implements OnInit {
 
   ratecard = new RateCard();
 
+  global = false;
+  isSuperAdmin = false;
+
   constructor(private api: RateCardApiService,
-    private route: ActivatedRoute) { }
+    private superAdminApi: SuperAdminApiService,
+    private route: ActivatedRoute,
+    private dialog: DialogService,
+    private router: Router,
+    private notifications: NotificationService) { }
 
   ngOnInit() {
-    this.route.data.subscribe((data: { rateCard: RateCard }) => {
+    this.route.data.subscribe((data: { rateCard: RateCard, global: boolean, superAdmin: boolean }) => {
       this.ratecard = data.rateCard;
+      this.global = data.global;
+      this.isSuperAdmin = data.superAdmin;
     });
   }
 
@@ -54,5 +66,55 @@ export class RateCardDetailsComponent implements OnInit {
     }
 
     return false;
+  }
+
+  deleteRateCard(ratecard: RateCard) {
+    this.dialog.confirmDeletion("Are you sure you want to delete this Rate Card?").subscribe(confirm => {
+      if (!confirm)
+        return;
+
+      let base: Observable<any>;
+
+      if (this.isSuperAdmin) {
+        base = this.superAdminApi.deleteGlobalRateCard(ratecard);
+      }
+      else base = this.api.deleteRateCard(ratecard);
+
+      base.subscribe(
+        data => {
+          if (data.success) {
+            this.router.navigateByUrl(this.isSuperAdmin ? '/superadmin/ratecards' : '/ratecards');
+          }
+          else {
+            console.log(data);
+
+            this.notifications.show(data.msg);
+          }
+        },
+        err => console.log(err)
+      );
+    });
+  }
+
+  private navigateToReleaseOrder(ratecard: RateCard) {
+    this.router.navigate(['/releaseorders/fromRateCard', ratecard.id]);
+  }
+
+  createReleaseOrder(ratecard: RateCard) {
+    if (ratecard.validTill && ratecard.validTill < new Date()) {
+      this.dialog.showYesNo('Rate Card Expired', 'This Rate Card has expired, Do you wish to continue?').subscribe(result => {
+        if (result) {
+          this.navigateToReleaseOrder(ratecard);
+        }
+      });
+    }
+    else if (ratecard.validTill && new Date(ratecard.validTill).setDate(-30) < Date.now()) {
+      this.dialog.showYesNo('Rate Card Expired', 'This Rate Card is about to expire, Do you wish to continue?').subscribe(result => {
+        if (result) {
+          this.navigateToReleaseOrder(ratecard);
+        }
+      });
+    }
+    else this.navigateToReleaseOrder(ratecard);
   }
 }
