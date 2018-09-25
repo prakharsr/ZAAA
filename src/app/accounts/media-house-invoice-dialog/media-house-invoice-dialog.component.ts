6eec9a936fe5a9147bb27ef1fa5fab46ca27fa6c
Mathew Sachin <mathew.sachin.git@outlook.com>
2018-09-25 18:20:30 +0530

    mhi tax, amount autofill

--- src/app/accounts/media-house-invoice-dialog/media-house-invoice-dialog.component.ts
+++ src/app/accounts/media-house-invoice-dialog/media-house-invoice-dialog.component.ts
@@ -12,19 +12,20 @@ export class MediaHouseInvoiceDialogComponent implements OnInit {
 
   details = new MediaHouseInvoice();
 
+  totalAmount = 0;
+  totalTax = 0;
+
   constructor(@Inject(MAT_DIALOG_DATA) public data: { ro: ReleaseOrder, insertions }) {
     this.details.releaseOrderId = data.ro.id;
     this.details.insertions = data.insertions;
+
+    this.totalAmount = this.details.insertions.reduce((a, b) => a + b.netAmount, 0);
+    this.totalTax = this.details.insertions.reduce((a, b) => a + b.taxAmount, 0);
+
+    this.details.MHIGrossAmount = this.totalAmount;
+    this.details.MHITaxAmount = this.totalTax;
   }
 
   ngOnInit() {
-  }
-
-  get totalAmount() {
-    return this.details.insertions.reduce((a, b) => a + b.netAmount, 0);
-  }
-
-  get totalTax() {
-    return this.details.insertions.reduce((a, b) => a + b.taxAmount, 0);
   }
 }
